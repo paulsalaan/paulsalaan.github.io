4d6d19664336d3eab36df338673f099f68d17a74
paulsalaan <paulninosalaan17@gmail.com>
2025-04-17 16:54:30 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+      },
+      colors: {
+        linkedin: "#04042D",
+        iceblack: "#01010B",
+        snowwhite: "#F8F8FF",
+        lightgray: "#9E9E9E",
+        mediumgray: "#464646",
+        darkgray: "#1F1F1F",
+        blackgray: "#090909",
+        darkgreen: "#041A08",
+      },
+    },
+  },
+  plugins: [],
+};
